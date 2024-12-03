4d058b9cd20eb9f71720278b0d8a95e39836349f
Romus Shrestha <sromus498@gmail.com>
2024-12-03 13:38:30 +0545

    created auth services and db services for appwrite

---
+++ src/appwrite/config.js
@@ -0,0 +1,9 @@
+import conf from '../conf/conf'
+import { Client, ID, Databases, Storage, Query } from 'appwrite'
+
+export class Service {
+
+}
+const service = new Service()
+
+export default service
